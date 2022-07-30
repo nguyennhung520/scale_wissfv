1d7bd3f5fa9e68c6dd82d5957b62cc495c755602
khanh245kh <kn0673342@gmail.com>
2022-07-30 20:40:49 +0700

    khanhnn

--- js/script.js
+++ js/script.js
@@ -122,3 +122,33 @@
   });
 
 })(jQuery);
+
+$(function () {
+  $('.toggle-menu').click(function(){
+   $('.exo-menu').toggleClass('display');
+   
+  });
+  
+ });
+ let navbarlinks = document.querySelectorAll('#navbar .scrollto');
+
+ function navbarlinksActive() {
+   navbarlinks.forEach(navbarlink => {
+
+     if (!navbarlink.hash) return;
+
+     let section = document.querySelector(navbarlink.hash);
+     if (!section) return;
+
+     let position = window.scrollY;
+     if (navbarlink.hash != '#header') position += 200;
+
+     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+       navbarlink.classList.add('active');
+     } else {
+       navbarlink.classList.remove('active');
+     }
+   })
+ }
+ window.addEventListener('load', navbarlinksActive);
+ document.addEventListener('scroll', navbarlinksActive);
